4a922154c146938374d499d75c8ff69e36d202e8
obaychu <olivia.baychu@edu.sait.ca>
2023-12-04 13:21:41 -0700

    button

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 // Add an event listener to the button with the ID "btn-alert"
 document.getElementById('btn-alert').addEventListener('click', function() {
     // Display an alert when the button is clicked
-    alert('Click the Link on the Left');
+    alert('see you next time');
 });
  
 // Get the button element by ID
@@ -13,11 +13,11 @@ const button = document.getElementById('btn-alert');
  
 button.addEventListener('mouseover', function() {
     // Change the button label on mouseover
-    button.innerText = 'Click the Link on the left';
+    button.innerText = 'See you next time';
 });
  
  
 button.addEventListener('mouseleave', function() {
     // Revert the button label when not hover
-    button.innerText = 'Wanna hear from me?';
+    button.innerText = 'Thanks for stopping by';
 });
