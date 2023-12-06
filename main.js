329e834c105dcd671c94b38f607240acd41d66c6
obaychu <olivia.baychu@edu.sait.ca>
2023-12-05 19:31:24 -0700

    If else

--- main.js
+++ main.js
@@ -21,3 +21,35 @@ button.addEventListener('mouseleave', function() {
     // Revert the button label when not hover
     button.innerText = 'Thanks for stopping by';
 });
+
+let count = 1;
+
+document.getElementById('btn-counter').onclick = () => {
+    count = count + 1;
+    document.getElementById('txt-counter').innerHTML = `Number: ${count}`;
+
+    // Check if the number is even or odd
+    if (count % 2 === 0) {
+        // Even number
+        document.getElementById('txt-counter').classList.remove('odd');
+        document.getElementById('txt-counter').classList.add('even');
+    } else {
+        // Odd number
+        document.getElementById('txt-counter').classList.remove('even');
+        document.getElementById('txt-counter').classList.add('odd');
+    }
+}
+    
+for (let i = 1; i <= 100; i++) {
+    let newLi = document.createElement("li");
+    newLi.innerText = i;
+
+    if (i % 2 === 0) {
+        newLi.innerText += " (even)";
+    } else {
+        newLi.innerText += " (odd)";
+    }
+
+    document.getElementById('numbers').appendChild(newLi);
+}
+
